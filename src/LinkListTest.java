6ab50953d11c52891498f359987df252ede5d2dc
kevinsmith <kevinsmith2001233@gmail.com>
2022-04-21 20:04:18 +0800

    888

--- src/LinkListTest.java
+++ src/LinkListTest.java
@@ -1,5 +1,10 @@
 public class LinkListTest {
     public static void main(String[] args) {
-
+        LinkList list = new LinkList();
+        for(int i=0;i<100;i++)
+            list.addAtHeader(i);
+        list.removeAtHeader();
+        System.out.println("search(99) is"+list.search(99));
+        System.out.println("list count="+list.getCount());
     }
 }
