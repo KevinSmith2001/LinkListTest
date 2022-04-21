47b3a4b64ce5bdd5e84aaa73e77c667cec022029
kevinsmith <kevinsmith2001233@gmail.com>
2022-04-21 19:38:32 +0800

    helloworld

--- src/LinkList.java
+++ src/LinkList.java
@@ -3,17 +3,30 @@ public class LinkList {
     public LinkList(){
         head = null;
     }
+    //1.初始化链表
     public void clearList(){
         head = null;
     }
+    //2.头部增加节点
     public void addAtHeader(int x){
         Node n = new Node(x);
         n.setNext(head);
         head = n;
     }
+    //3.头部删除节点
     public void removeAtHeader(){
         if(head!= null)
             head= head.getNext();
+    }
+    //4.统计链表节点数量,链表遍历
+    public int getCount(){
+        Node p = head;
+        int count=0;
+        while (p!=null){
+            count = count +1;
+            p=p.getNext();
+        }
+        return count;
     }
 
 }
